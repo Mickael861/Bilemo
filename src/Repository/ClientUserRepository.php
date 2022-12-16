b0d3bcdbca1c83dbcd13f1a0b4abb5420e4362b6
Mickael861 <m-iicka86@hotmail.fr>
2022-12-16 13:46:00 +0100

    fixes#24, correction symfonyInsight

--- src/Repository/ClientUserRepository.php
+++ src/Repository/ClientUserRepository.php
@@ -38,29 +38,4 @@ class ClientUserRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
-
-//    /**
-//     * @return ClientUser[] Returns an array of ClientUser objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('c.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?ClientUser
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
